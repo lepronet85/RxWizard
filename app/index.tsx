c59f1cb181168205f4aa50c786f8c4145a333b83
Abdoul <lepronet85@gmail.com>
2024-05-28 16:56:01 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useRef } from "react";
+import LottieView from "lottie-react-native";
+
+const Page = () => {
+  const animation = useRef(null);
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/onboarding.jpg")}
+        style={styles.img}
+      />
+      <LottieView
+        autoPlay
+        loop
+        ref={animation}
+        style={{
+          width: 200,
+          height: 100,
+        }}
+        source={require("../assets/animation/loading.json")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    width: 300,
+    height: 300,
+  },
+});
+
+export default Page;
