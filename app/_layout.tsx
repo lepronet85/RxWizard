1b93c05d9ab16ca413cff676daa0cdda0c8dcabc
Abdoul <lepronet85@gmail.com>
2024-06-12 09:35:45 +0100

    Add comments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,31 @@
 import { Stack } from "expo-router";
 
+// Root layout component for the app's navigation stack
 export default function RootLayout() {
   return (
     <Stack>
+      {/* Index screen */}
       <Stack.Screen
         name="index"
         options={{
           headerShown: false,
         }}
       />
+      {/* Home screen */}
       <Stack.Screen
         name="home"
         options={{
           headerShown: false,
         }}
       />
+      {/* Product detail screen */}
       <Stack.Screen
         name="product-detail"
         options={{
           headerShown: false,
         }}
       />
+      {/* Chat screen */}
       <Stack.Screen
         name="chat"
         options={{
